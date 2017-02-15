ad1ca737c8e71d9ffda7eb470567dd8e20eab735
githinhub <hpvong@mail.sfsu.edu>
2017-02-15 01:10:42 -0800

    Add files via upload
    http://sfsuse.com/~hpvong/cake/about.php

---
+++ about.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <title>About</title>
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="site.css">
+
+    </head>
+    <body>
+        <style>
+            p { 
+   
+left:500px;
+top:40px;
+color:#000000;
+}
+                 body {
+                     
+  
+    background-image:url(images/Assassin.jpg);
+      background-size:1400px 900px ; 
+    background-repeat: no-repeat;
+}
+        li{
+        display: inline;
+        }
+        div.fixed {
+    position: fixed;
+    bottom: 300px;
+    left: 250px;
+    width: 400px;
+    border: 7px solid #282121;
+}
+        </style>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+    <div class="navbar-header">
+    </div>
+            </div>
+        </nav>
+        <div>
+            <div class = "fixed">
+               <font size="5"> Hin Vong goes to San Francisco State University </font>
+                
+                
+            </div>
+        </div>
+    </body>
+</html>
