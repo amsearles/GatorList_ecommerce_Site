f53c502aabae628f65e8db58bb1c66dfa7b01bd9
NiftiyevHasan <hasan.niftiyev@yahoo.com>
2017-02-17 19:27:33 -0800

    We moved t11 to parent directory , public_html

---
+++ t11/webroot/about_team.php
@@ -0,0 +1,134 @@
+<html>
+    <html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <meta charset="utf-8">
+        <title>Team 11</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <meta name="generator" content="Bootply" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="" />
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="apple-touch-icon" href="/bootstrap/img/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x114.png">
+        
+<head>
+	<title>ZDR</title>
+	
+	<!--<link rel="stylesheet" type="text/css" href="buttons.css">
+	<link rel="stylesheet" type="text/css" href="images.css">-->
+</head>
+
+	
+    <div id="wrap">
+  
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Team 11</a> 
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="index.php">Home</a></li>
+          <li class = "active"><a href="about_team.php">About</a></li>
+          <li class=""><a href = "index.php"><em class="">Buy & Sell SFSU TextBooks</em></a></li>
+          </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+  
+  <!-- Begin page content -->
+ </div> 
+
+    
+<body>	
+    <div class="hello">
+        
+    </div>
+	<section>
+		<div class="container">
+			<div class="left" style="text-align: center">	
+                            <h1> Team 11 - 648 Software Engineering Project - Spring 17</h1>
+				<div class="imageWrapper">  
+                                    <center>
+                                    <table>
+                                        <tr>
+                                        <td>
+                                            <a href="jordan.php">
+                                                <img src="./img/linkedin.jpg" class="photos"  ></a>
+                                                <div class="details">
+                                                    <span class="name" >Jordan Scandlyn</span><br>
+                                                    <span class="personal">Since '94</span>
+                                                </div>
+                                        </td>
+                                        <td>
+                                            <a href="about.php">
+                                                <img src="./img/belli.jpg" class="photos"></a>
+                                                <div class="details">
+                                                    <span class="name" >Hasan Niftiyev</span><br>
+                                                    <span class="personal">Filler text til y'all decide on if this is even right design </span>
+                                                </div>
+                                        </td>
+                                        <td>
+                                            <a href="daryl.php">
+                                                <img src="http://i.imgur.com/fcjW6OY.png" class="photos"></a>
+                                                <div class="details">
+                                                    <span class="name" >Daryl Robbin</span><br>
+                                                    <span class="personal">Filler text</span>
+                                                </div>
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td>
+                                            <a href="searles.php">
+                                                <img src="./img/searles.jpg" class="photos"  ></a>
+                                                <div class="details">
+                                                    <span class="name" >Anthony Searles</span><br>
+                                                    <span class="personal">Here is some filler text </span>
+                                                </div>
+                                        </td>
+                                        <td>
+                                            <a href="about.php">
+                                                <img src="./img/belli.jpg" class="photos"></a>
+                                                <div class="details">
+                                                    <span class="name" >Andre Simpelo</span><br>
+                                                    <span class="personal">This is filler text</span>
+                                                </div>
+                                        </td>
+                                        <td>
+                                            <a href="hin.php">
+                                                <img src="./img/Hin.jpg" class="photos"></a>
+                                                <div class="details">
+                                                    <span class="name" >Hin Vong</span><br>
+                                                    <span class="personal">Filling space til we come up with good idea </span>
+                                                </div>
+                                        </td>
+                                        </tr>
+                                       
+                                    </table>
+                                    </center>
+				</div>
+				
+		
+                  	</div>
+             		
+				</div><br>
+               
+			
+			<div class="right">
+				
+			</div>
+		
+	</section>
+    </body>
+  
+
+</html>
+
