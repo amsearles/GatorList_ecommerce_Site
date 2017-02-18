81bc39b766a7ff5fc55a98ebd7ad7cecc6b17850
NiftiyevHasan <hasan.niftiyev@yahoo.com>
2017-02-17 21:08:32 -0800

    Updated Main page

--- webroot/index.php
+++ webroot/index.php
@@ -56,6 +56,8 @@
 		<div class="container">
 			<div class="left" style="text-align: center">	
                             <h1> Team 11 - 648 Software Engineering Project - Spring 17</h1>
+                            <a href="index.php">
+                                                <img src="./img/programming.gif" class="personal_page_photos"  ></a>
 				
 		
                   	</div>
