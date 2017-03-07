c132c30c2d25b0b367988a9c44a768b78f506c15
NiftiyevHasan <hasan.niftiyev@yahoo.com>
2017-03-07 10:36:12 -0800

    Changed Tree Structure According to Anthony Souza's input

---
+++ GatorList/webroot/index.php
@@ -0,0 +1,77 @@
+<html>
+    <html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <meta charset="utf-8">
+        <title>Team 11</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <meta name="generator" content="Bootply" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="" />
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="apple-touch-icon" href="/bootstrap/img/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x114.png">
+        
+<head>
+	<title>ZDR</title>
+	
+	<!--<link rel="stylesheet" type="text/css" href="buttons.css">
+	<link rel="stylesheet" type="text/css" href="images.css">-->
+</head>
+
+	
+    <div id="wrap">
+  
+  <!-- Fixed navbar -->
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Team 11</a> 
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class=""><a href="index.php">Home</a></li>
+          <li class = "active"><a href="about.php">About</a></li>
+          <li class=""><a href = "index.php"><em class="">Buy & Sell SFSU TextBooks</em></a></li>
+          </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+  
+  <!-- Begin page content -->
+ </div> 
+
+    
+<body>	
+    <div class="hello">
+        
+    </div>
+	<section>
+		<div class="container">
+			<div class="left" style="text-align: center">	
+                            <h1> Team 11 - 648 Software Engineering Project - Spring 17</h1>
+                            <a href="index.php">
+                                                <img src="./img/programming.gif" class="personal_page_photos"  ></a>
+				
+		
+                  	</div>
+             		
+				</div><br>
+               
+			
+			<div class="right">
+				
+			</div>
+		
+	</section>
+    </body>
+  
+
+</html>
+
