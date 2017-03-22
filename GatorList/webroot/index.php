41d3cb44963969ab5c5ab095e9fc454e97162ece
NiftiyevHasan <hasan.niftiyev@yahoo.com>
2017-03-21 23:12:18 -0700

    Updated M2 doc and re-structured

--- GatorList/webroot/index.php
+++ GatorList/webroot/index.php
@@ -1,77 +1,37 @@
-<html>
-    <html lang="en">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
-        <meta charset="utf-8">
-        <title>Team 11</title>
-        <link rel="stylesheet" type="text/css" href="./css/style.css">
-        <meta name="generator" content="Bootply" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <meta name="description" content="" />
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="apple-touch-icon" href="/bootstrap/img/apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/bootstrap/img/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/bootstrap/img/apple-touch-icon-114x114.png">
-        
-<head>
-	<title>ZDR</title>
-	
-	<!--<link rel="stylesheet" type="text/css" href="buttons.css">
-	<link rel="stylesheet" type="text/css" href="images.css">-->
-</head>
+<?php
+/**
+ * The Front Controller for handling every request
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+// for built-in server
+if (php_sapi_name() === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
 
-	
-    <div id="wrap">
-  
-  <!-- Fixed navbar -->
-  <div class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php">Team 11</a> 
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class=""><a href="index.php">Home</a></li>
-          <li class = "active"><a href="about.php">About</a></li>
-          <li class=""><a href = "index.php"><em class="">Buy & Sell SFSU TextBooks</em></a></li>
-          </ul>
-      </div><!--/.nav-collapse -->
-    </div>
-  </div>
-  
-  <!-- Begin page content -->
- </div> 
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-    
-<body>	
-    <div class="hello">
-        
-    </div>
-	<section>
-		<div class="container">
-			<div class="left" style="text-align: center">	
-                            <h1> Team 11 - 648 Software Engineering Project - Spring 17</h1>
-                            <a href="index.php">
-                                                <img src="./img/programming.gif" class="personal_page_photos"  ></a>
-				
-		
-                  	</div>
-             		
-				</div><br>
-               
-			
-			<div class="right">
-				
-			</div>
-		
-	</section>
-    </body>
-  
+use App\Application;
+use Cake\Http\Server;
 
-</html>
+// Bind your application to the server.
+$server = new Server(new Application(dirname(__DIR__) . '/config'));
 
+// Run the request/response through the application
+// and emit the response.
+$server->emit($server->run());
