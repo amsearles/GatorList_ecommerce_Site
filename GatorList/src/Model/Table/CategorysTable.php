cc6e6cb2b8ff09c337ba46671edb46127d1a6e1c
amsearles <asearles@mail.sfsu.edu>
2017-04-09 04:38:43 +0000

    adding entity and table classes

---
+++ GatorList/src/Model/Table/CategorysTable.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class CategorysTable extends Table
+{
+
+    public function initialize(array $config)
+    {
+        $this->setTable('categories');
+
+        $this->setPrimaryKey('category_id');
+	
+    }
+
+}
