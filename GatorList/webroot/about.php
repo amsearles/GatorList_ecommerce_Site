8b91ee2684b75dbee91fbca6267550c9e6669e59
dmrobbin <dmrobbin@mail.sfsu.edu>
2017-04-17 21:42:27 -0700

    [about.php] nav bar

--- GatorList/webroot/about.php
+++ GatorList/webroot/about.php
@@ -1,5 +1,3 @@
-
-
 <html>
 <head>
     <meta charset="utf-8"/>    <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +12,7 @@
         <ul class="title-area large-3 medium-4 rows">
             <li class="name">
                 <h1>
-                <a href="http://sfsuse.com/~sp17g11/about.php">About Us</a>
+                <a href="http://sfsuse.com/~sp17g11/">Gator List</a>
                 </h1>
             </li>
             
@@ -22,25 +20,33 @@
         </ul>
         <div class="top-bar-section">
             <ul class="left">
-		<li><a href="http://sfsuse.com/~sp17g11/">Home</a></li>
-                <li><a href="http://sfsuse.com/~sp17g11/items">Buy</a></li>
-                <li><a href="http://sfsuse.com/~sp17g11/items/add">Sell</a></li>
-                <li><a href="http://sfsuse.com/~sp17g11/about.php">About Us</a> </li>  
-                <li><a href="http://sfsuse.com/~sp17g11/items"> Search</a></li>
+                <li><a href="http://sfsuse.com/~sp17g11/">Home</a></li>
+                <li><a href="http://sfsuse.com/~sp17g11/items">Browse</a></li>
+                <li><a href="http://sfsuse.com/~sp17g11/items/add">Sell</a></li>               
+                 <!-- catagories -->
+                 <li>
+             <select name="category">
+                 <option value="4">Apparel</option>
+                 <option value="3">Books</option>
+                <option value="1">Electronics</option>
+                <option value="2">Furniture</option>     
+             </select>
+                </li>
                 <li>
                 <div class="search">    
             <form action="" method="post">
             
                 <input type ="text" name="submit"/>
-           
+                
             </form>
                     
             </li>
-            <button>Submit  </button>
+            <button>Search  </button>
             </ul>
             <ul class ="right">
                 <li><a target="_blank" href="http://sfsuse.com/~sp17g11/login.php">Login</a></li>
                 <li><a target="_blank" href="http://sfsuse.com/~sp17g11/register.php">Register</a></li>
+                <li><a href="http://sfsuse.com/~sp17g11/about.php">About Us</a> </li> 
                  <li><a target="_blank" href="http://api.cakephp.org/3.0/">API</a></li>
             </ul>
         </div>
